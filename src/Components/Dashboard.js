73d6472cc2f9da2c524bee879d7a27fd03675c5b
ranaadeel15911 <ra0511083@gmail.com>
2024-01-11 20:25:21 +0500

    fajf

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -8,7 +8,7 @@ const Dashboard = () => {
   const getUser = async () => {
     try {
         // const response = await axios.get("http://localhost:900/login/sucess", { withCredentials: true });
-        const response = await axios.get("http://o-auth-server-kappa.vercel.app/login/sucess", { withCredentials: true });
+        const response = await axios.get("https://o-auth-server-kappa.vercel.app/login/sucess", { withCredentials: true });
 
         console.log("response",response)
     } catch (error) {
