f5c74c5a46612fbded73d64eb736a4689326773f
Rana adeel <112337492+ranaadeel15911@users.noreply.github.com>
2024-01-12 12:02:24 +0500

    Update Dashboard.js

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,9 +1,25 @@
 import axios from 'axios';
 import React, { useEffect } from 'react'
-// import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Dashboard = () => {
- 
+  const navigate = useNavigate();
+
+  const getUser = async () => {
+    try {
+        const response = await axios.get("https://o-auth-server-kappa.vercel.app/login/sucess");
+
+        console.log("response",response)
+    } catch (error) {
+      // alert(error.response.data.message)
+      // console.log(error.response.data.message)
+      navigate("/*")
+    }
+}
+
+useEffect(() => {
+  getUser()
+}, [])
   return (
     <div>Dashboard</div>
   )
